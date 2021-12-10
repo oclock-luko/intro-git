15e6a97fe9fa111d2e8c89e41b35d318300cd966
oclock-luko <alexandre.mouillard@oclock.io>
2021-12-10 10:14:58 +0000

    Michelisation du code

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.write('Hello, GIT !');
+    res.write('Bonjour, GIT !');
     res.end();
 });
 
 app.get('/hello/:name', (req, res) => {
-    res.write(`Hello, ${req.params.name}`);
+    res.write(`Bonjour, ${req.params.name}`);
     res.end();
 });
 
 app.get('/goodbye', (req, res) => {
-    res.write('Goodbye');
+    res.write('Au revoir');
     res.end();
 });
 
