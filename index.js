b6aefddd09d3f11e92334569e83bee6baeb334fc
oclock-luko <alexandre.mouillard@oclock.io>
2021-12-10 09:54:42 +0000

    Add goodbye route

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/hello/:name', (req, res) => {
     res.end();
 });
 
+app.get('/goodbye', (req, res) => {
+    res.write('Goodbye');
+    res.end();
+});
+
 app.listen(3000, _ => {
     console.log(`App listening on http://localhost:3000`);
 });
